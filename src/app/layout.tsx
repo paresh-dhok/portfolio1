b36a08d307cbf8f1f35aa6cf166e913b9d8f3b26
paresh-dhok <pareshdhok20@gmail.com>
2025-05-19 21:52:59 +0530

    Remove chatbot functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "sonner";
 import "@/app/globals.css";
 import Script from "next/script";
-import ChatBotWidget from "@/components/ChatBotWidget";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -39,7 +38,6 @@ export default function RootLayout({
         <ThemeProvider>
           {children}
           <Toaster />
-          <ChatBotWidget />
         </ThemeProvider>
       </body>
     </html>
